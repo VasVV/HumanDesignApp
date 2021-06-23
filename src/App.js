a8ffe5d5137ec8f1de8e7ed5c9dbe53042ae805e
VasVV <radioheadboy0@gmail.com>
2021-06-23 17:53:21 +0200

    fixes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+// import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import {connect} from 'react-redux';
 import Particles from 'react-particles-js';
--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
-import '../node_modules/font-awesome/css/font-awesome.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import { createStore } from 'redux';
 import allReducers from './reducers'; //auto look for index.js
