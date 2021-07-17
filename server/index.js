9433ec0d080a02dd51f798841b5dacea06b3a815
VasVV <radioheadboy0@gmail.com>
2021-07-17 19:54:07 +0200

    fixes

--- server/index.js
+++ server/index.js
@@ -19,6 +19,14 @@ const stripe = new Stripe(
 
 var bodyParser = require("body-parser");
 
+app.use(function (req, res, next){
+  if (req.headers['x-forwarded-proto'] === 'https') {
+    res.redirect('http://' + req.hostname + req.url);
+  } else {
+    next();
+  }
+});
+
 
 
 app.use(bodyParser({ limit: "4MB" }));
